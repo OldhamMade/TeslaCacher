ffc95660d5d071f2070c99e5c1523ec75337b4e3
Phillip Oldham <phillip.oldham@gmail.com>
2020-10-31 13:17:20 +0100

    Remove elixir1.6 from testing, add fix for 1.7 & 1.8

--- test/tesla_cacher_test.exs
+++ test/tesla_cacher_test.exs
@@ -5,7 +5,7 @@ defmodule Tesla.Middleware.CacherTest do
   @expiry_sleep 111
 
   setup do
-    redix_uri = System.get_env("REDIS_URI", "redis://localhost:6379")
+    redix_uri = System.get_env("REDIS_URI") || "redis://localhost:6379"
 
     {:ok, redix_pid} = Redix.start_link(redix_uri, name: :redix)
     {:ok, agent_pid} = Agent.start_link(fn -> 0 end, name: :http_call_count)
